5aef84c06b4259571a22b4100f54177047938d66
Javier Cacheiro <jlopez@cesga.es>
2016-06-13 15:15:07 +0200

    increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='configuration-registry',
-    version='0.1.18',
+    version='0.1.19',
     author='Jonatan Enes & Javier Cacheiro',
     author_email='bigdata-dev@listas.cesga.es',
     url='https://github.com/javicacheiro/configuration-registry',
