fbf76f93961d570ba8a3d50580e9a6c8f363ceb7
JonatanEnes <jonatan.enes@protonmail.com>
2016-06-20 18:50:14 +0200

    Se sube version y se publica de nuevo en PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='configuration-registry',
-    version='0.1.24',
+    version='0.1.25',
     author='Jonatan Enes & Javier Cacheiro',
     author_email='bigdata-dev@listas.cesga.es',
     url='https://github.com/javicacheiro/configuration-registry',
