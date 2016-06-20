259b44bd19f0f7db8277fbd86412a45c32545002
Javier Cacheiro <jlopez@cesga.es>
2016-06-20 11:40:30 +0200

    fixed dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='configuration-registry',
-    version='0.1.23',
+    version='0.1.24',
     author='Jonatan Enes & Javier Cacheiro',
     author_email='bigdata-dev@listas.cesga.es',
     url='https://github.com/javicacheiro/configuration-registry',
@@ -12,7 +12,7 @@ setup(
     description='Python Resource Allocation API',
     long_description=open('README.rst').read(),
     py_modules=['registry'],
-    install_requires=['requests', 'PyYAML'],
+    install_requires=['kvstore', 'requests', 'jinja2', 'PyYAML', 'futures'],
     test_suite='tests',
     classifiers=[
         'License :: OSI Approved :: MIT License',
