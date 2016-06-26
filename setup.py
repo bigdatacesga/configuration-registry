b5029f423e62868eaf55e047d0c629e99dac232a
JonatanEnes <jonatan.enes@protonmail.com>
2016-06-27 01:06:30 +0200

    Se sube la version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='configuration-registry',
-    version='0.2.5',
+    version='0.2.6',
     author='Jonatan Enes & Javier Cacheiro',
     author_email='bigdata-dev@listas.cesga.es',
     url='https://github.com/javicacheiro/configuration-registry',
